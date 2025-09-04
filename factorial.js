1a39bb468540821aa42a5faf1863d2e9cd9c8092
divinomeya <divinomeya@gmail.com>
2025-09-04 10:29:41 +0800

    Added readline Interface

--- factorial.js
+++ factorial.js
@@ -1,3 +1,10 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
 console.log("Factorial Application");
 
 console.log("1. Sat Hello");
