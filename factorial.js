c0ddd841dbd0718a0a3051ad79c79c8d73a9aa47
divinomeya <divinomeya@gmail.com>
2025-09-08 09:10:29 +0800

    Added backToMenuChoice

--- factorial.js
+++ factorial.js
@@ -16,6 +16,24 @@ rl.question("Enter your choice (1-3" , choice =>{
         console.log("Hello There!");
     } else if (choice=="2"){
         console.log("Factorial");
+        rl.question("enter a number for factorial: ", (numStr) =>{
+         let num =preseInt(munStr)
+
+         if(isMainThread(num) || num<0){
+            console.log("plaese enter a non negative integer");
+         } else {
+
+            let fact =1;
+
+            for (let i = 1; i <= num ; i++){
+                fact *=i;
+            }
+
+            console.log("The factorial of " + num + "is" + fact);
+
+         }
+     });
+
     } else if (choice=="3"){
         console.log("Exit");
     } else {
