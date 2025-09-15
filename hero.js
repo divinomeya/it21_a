dbc42f8fa0230e192490ffd6b327d68c16af85f6
divinomeya <divinomeya@gmail.com>
2025-09-15 09:14:13 +0800

    Added hero.js, warrior subclass, mage subclass, and updated initial object

---
+++ hero.js
@@ -0,0 +1,37 @@
+class Hero{
+      #health;
+
+    constructor (name, health, attack){
+        this.name = name;
+        this.#health = health;
+        this.attack = attack;
+    }
+
+     getHealth(){
+        return this.#health;
+    }
+}
+
+class Warrior extends Hero{
+    useAbility(){
+        console.log(`${this.name} uses power Strike`);
+    }
+} 
+
+class Mage extends Hero{
+    constructor(name,health,attack,mana){
+        super(name, health, attack);
+        this.mana = mana;
+    }
+    useAbility(){
+        console.log(`${this.name} casts Fireball`);
+    }
+}
+
+
+const warrior = new Warrior("Thorin", 100,10);
+warrior.useAbility();
+
+const mage = new Mage("Gandall", 80, 8, 50);
+mage.useAbility();
+
